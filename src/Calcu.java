4b1bdec6078ed3e24a583ed14f4e0b58db83d05a
Ivette <car16020@uvg.edu.gt>
2017-01-31 16:47:58 -0600

    Buffer

--- src/Calcu.java
+++ src/Calcu.java
@@ -4,23 +4,23 @@ public class Calcu implements iCalculadora {
 	
 	@Override
 	public int suma(int a, int b){
-		return 1;
+		return (a+b);
 	}
 	
 	
 	@Override
 	public int resta(int a, int b){
-		
+		return (a-b);
 	}
 	
 	@Override
 	public int multiplicacion(int a, int b){
-		
+		return (a*b);
 	}
 	
 	@Override
-	public int division(int a, int b);{
-		
+	public int division(int a, int b){
+		return (a/b);
 	}
 
 }
--- src/PostfixC.java
+++ src/PostfixC.java
@@ -1,4 +1,7 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 /**
  * @author chavez
  *
@@ -11,10 +14,52 @@ public class PostfixC {
 	public static void main(String[] args) {
 		
 		iPila<Integer> miPila = new PilaArray<Integer>();
-		
-		
-		
-		
+		iCalculadora miCalculadora = new Calcu();
+		 String cadena;
+		 int contador;
+		 int a;
+		 int c;
+		 contador = 0;
+		 	
+		 	FileReader fr = new FileReader("datos.txt");
+		 	BufferedReader bf = new BufferedReader(fr);
+	        while((cadena = Integer.toString(bf.read()))!=null) {
+	            if (contador != 2) {
+	            	miPila.push(Integer.parseInt(cadena));
+	            	contador++;
+	            }
+	            else if (contador == 2){
+	            	if (cadena.equals("+")){
+	            		c = miPila.pop();
+	            		a = miPila.pop();
+	            		int resultado = miCalculadora.suma(a,c);
+	            		miPila.push(resultado);
+	            		contador = 1;
+	            	}
+	            	if (cadena.equals("-")){
+	            		c = miPila.pop();
+	            		a = miPila.pop();
+	            		int resultado = miCalculadora.resta(a,c);
+	            		miPila.push(resultado);
+	            		contador = 1;
+	            	}
+	            	if (cadena.equals("*")){
+	            		c = miPila.pop();
+	            		a = miPila.pop();
+	            		int resultado = miCalculadora.multiplicacion(a,c);
+	            		miPila.push(resultado);
+	            		contador = 1;
+	            	}
+	            	if (cadena.equals("/")){
+	            		c = miPila.pop();
+	            		a = miPila.pop();
+	            		int resultado = miCalculadora.division(a,c);
+	            		miPila.push(resultado);
+	            		contador = 1;
+	            	}
+	            }
+	        }
+	        b.close();	
 	}
 
 }
